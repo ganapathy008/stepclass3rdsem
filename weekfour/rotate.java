47f0477644dbb185d813a1c512b73185788e8c23
ganapathy008 <ganapathylakshmanan008@gmail.com>
2026-09-05 10:48:44 +0530

    Add files via upload

---
+++ weekfour/rotate.java
@@ -0,0 +1,17 @@
+package weekfour;
+
+public class rotate {
+    
+    public int[] rotateArray(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        int[] newArray = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            newArray[(i + k) % n] = nums[i];
+        }
+        
+        return newArray;
+    }
+}
+
