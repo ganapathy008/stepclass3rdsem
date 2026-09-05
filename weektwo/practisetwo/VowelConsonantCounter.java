a0170113a3576633830a579a230ab32d7058d78f
ganapathy008 <ganapathylakshmanan008@gmail.com>
2026-09-05 11:51:39 +0530

    Add files via upload

---
+++ weektwo/practisetwo/VowelConsonantCounter.java
@@ -0,0 +1,24 @@
+package weektwo.practisetwo;
+
+public class VowelConsonantCounter {
+
+    public static void countVowelsAndConsonants(String text) {
+        int vowels = 0, consonants = 0;
+        text = text.toLowerCase();
+
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                if ("aeiou".indexOf(ch) != -1) vowels++;
+                else consonants++;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels + " | Consonants: " + consonants);
+    }
+
+    public static void main(String[] args) {
+        countVowelsAndConsonants("Java Programming");
+    }
+}
+
